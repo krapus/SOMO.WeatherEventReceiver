43946b4ed56d732856f4ac1dd8f1dadfa7e43893
Dario Sierra <dariosierra9@gmail.com>
2022-03-26 12:23:38 -0500

    Stable version

---
+++ SOMO.WeatherEventReceiver/Models/WeatherMessage.cs
@@ -0,0 +1,10 @@
+﻿namespace SOMO.WeatherEventReceiver.Models
+{
+    internal class WeatherMessage
+    {
+        public string Location { get; set; }
+        public string Text { get; set; }
+        public string Icon { get; set; }
+        public int Code { get; set; }
+    }
+}
--- SOMO.WeatherEventReceiver/WeatherFunction.cs
+++ SOMO.WeatherEventReceiver/WeatherFunction.cs
@@ -1,16 +1,64 @@
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using SOMO.WeatherEventReceiver.Models;
+using System.Security.Authentication;
+using System.Threading.Tasks;
 
 namespace SOMO.WeatherEventReceiver
 {
     public static class WeatherFunction
     {
+        private static MongoClient MongoClient;
+
         [FunctionName("WeatherFunction")]
-        public static void Run([ServiceBusTrigger("weathereventmessages", Connection = "")]string myQueueItem, ILogger log)
+        public static async Task RunAsync([ServiceBusTrigger("weathereventmessages", Connection = "AzureBusConnectionstring")] string myQueueItem,
+            ILogger log, ExecutionContext context)
         {
+            var configuration = GetConnectionString(context);
+            var database = GetMongoClient(@configuration["CosmoConnectionString"]).GetDatabase("somo");
+
+            var collection = database.GetCollection<BsonDocument>("somo-weathers");
+            
+            await collection.InsertOneAsync(CreateDocument(JsonConvert.DeserializeObject<WeatherMessage>(myQueueItem)));
+
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+
+        private static IConfigurationRoot GetConnectionString(ExecutionContext context)
+        {
+            return new ConfigurationBuilder()
+             .SetBasePath(context.FunctionAppDirectory)
+             .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+             .AddEnvironmentVariables()
+             .Build();
+
+        }
+
+        private static MongoClient GetMongoClient(string connectionString)
+        {
+            if (MongoClient == null)
+            {
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString)
+                );
+                settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+                MongoClient = new MongoClient(settings);
+            }
+            return MongoClient;
+        }
+
+        private static BsonDocument CreateDocument(WeatherMessage message)
+        {
+            return new BsonDocument
+            {
+                {"location", message?.Location},
+                {"text", message?.Text },
+                {"icon", message?.Icon },
+                {"code", message?.Code }
+            };
         }
     }
 }
